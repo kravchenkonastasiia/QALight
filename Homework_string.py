8517297a62c0b5e889f968fbb2822d60768a6d1e
Anastasiia Kravchenko <kravchenkonastasiia@gmail.com>
2020-05-23 03:06:55 +0200

    Commit 2

---
+++ Homework_string.py
@@ -0,0 +1,22 @@
+x = 60.5
+y = 7.3
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+x = 50
+y = 20
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+greeting = "Hello"
+first_name = "Python"
+white_space = " "
+print(greeting + white_space + first_name)
+
+greeting = "Hello"
+first_name = "Python"
+print(greeting + " " + first_name)
